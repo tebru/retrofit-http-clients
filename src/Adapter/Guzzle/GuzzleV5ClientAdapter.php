bcb851c037bd73a08dbcbce400709fa2aee1c30e
epfremme <epfremme@nerdery.com>
2015-10-08 16:04:15 -0500

    Added async client adapter methods

--- src/Adapter/Guzzle/GuzzleV5ClientAdapter.php
+++ src/Adapter/Guzzle/GuzzleV5ClientAdapter.php
@@ -8,10 +8,15 @@ namespace Tebru\Retrofit\HttpClient\Adapter\Guzzle;
 
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Message\Request;
-use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\Promise\PromiseInterface;
+use GuzzleHttp\Psr7\Response as Psr7Response;
+use GuzzleHttp\Psr7\Request as Psr7Request;
+use GuzzleHttp\Ring\Future\FutureInterface;
 use GuzzleHttp\Stream\Stream;
 use Tebru;
 use Tebru\Retrofit\Adapter\HttpClientAdapter;
+use Tebru\Retrofit\Http\Callback;
 
 /**
  * Class GuzzleV5ClientAdapter
@@ -26,6 +31,11 @@ class GuzzleV5ClientAdapter implements HttpClientAdapter
      * @var ClientInterface
      */
     private $client;
+
+    /**
+     * @var PromiseInterface[]
+     */
+    private $promises = [];
 
     /**
      * Constructor
@@ -47,7 +57,7 @@ class GuzzleV5ClientAdapter implements HttpClientAdapter
      * @param string $uri
      * @param array $headers
      * @param string $body
-     * @return Response
+     * @return Psr7Response
      */
     public function send($method, $uri, array $headers = [], $body = null)
     {
@@ -57,12 +67,58 @@ class GuzzleV5ClientAdapter implements HttpClientAdapter
 
         $response = $this->client->send(new Request($method, $uri, $headers, $body));
 
-        return new Response(
+        return new Psr7Response(
             $response->getStatusCode(),
             $response->getHeaders(),
             $response->getBody(),
             $response->getProtocolVersion(),
             $response->getReasonPhrase()
         );
+    }
+
+    /**
+     * Send asynchronous guzzle request
+     *
+     * @param Psr7Request $request
+     * @param \Tebru\Retrofit\Http\Callback $callback
+     * @return null
+     */
+    public function sendAsync(Psr7Request $request, Callback $callback)
+    {
+        $request = new Request(
+            $request->getMethod(),
+            (string) $request->getUri(),
+            $request->getHeaders(),
+            $request->getBody(),
+            ['future' => true]
+        );
+
+        /** @var FutureInterface $response */
+        $response = $this->client->send($request);
+
+        $this->promises[] = $response
+            ->then(function (ResponseInterface $response) {
+                return new Psr7Response(
+                    $response->getStatusCode(),
+                    $response->getHeaders(),
+                    $response->getBody(),
+                    $response->getProtocolVersion(),
+                    $response->getReasonPhrase()
+                );
+            })
+            ->then($callback->success(), $callback->failure())
+        ;
+    }
+
+    /**
+     * Resolve all promises
+     *
+     * @return null
+     */
+    public function wait()
+    {
+        foreach ($this->promises as $promise) {
+            $promise->wait();
+        }
     }
 }
